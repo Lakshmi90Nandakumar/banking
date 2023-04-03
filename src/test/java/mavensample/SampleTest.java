078dbac7b901a7270ee67918f5fe3c3fb9d68610
LENOVO <LENOVO@DESKTOP-1ABT9ES>
2023-04-03 12:43:23 +0530

    banking 1st commmit command

---
+++ src/test/java/mavensample/SampleTest.java
@@ -0,0 +1,13 @@
+package mavensample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test(groups="regression")
+	public void test_01()
+	{
+		Reporter.log("welcome",true);
+	}
+}
